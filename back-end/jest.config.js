c8842150d195afd685f72c91a80e79f276b0d8c9
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-04 18:47:56 -0300

    chore: install jest

---
+++ back-end/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  // collectCoverage: true,
+  coverageProvider: 'babel',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
