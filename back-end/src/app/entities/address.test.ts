9ba0abc5a3b5bbf5ad8b734de91021ed30d9ce95
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-05 18:13:19 -0300

    feat: create addressValidInfo and export it

--- back-end/src/app/entities/address.test.ts
+++ back-end/src/app/entities/address.test.ts
@@ -1,4 +1,12 @@
-import { Address } from './Address'
+import { Address, IAddress } from './Address'
+
+export const addressValidInfo: IAddress = {
+  city: 'valid_city',
+  state: 'valid_state',
+  street: 'valid_street',
+  number: '22',
+  comment: 'valid_comment'
+}
 
 describe('Address', () => {
   describe('Successful tests', () => {
