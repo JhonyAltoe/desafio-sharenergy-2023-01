2da57fa27bd63676d2125ac9ec299ae8d5ffc240
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-05 18:56:39 -0300

    test: create validateCity

--- back-end/src/app/entities/address.test.ts
+++ back-end/src/app/entities/address.test.ts
@@ -1,5 +1,17 @@
 import { Address, IAddress } from './Address'
 
+class FctAddress {
+  addressProp: IAddress
+
+  constructor (addressProp: IAddress) {
+    this.addressProp = addressProp
+  }
+
+  execute (): Address {
+    return new Address(this.addressProp)
+  }
+}
+
 export const addressValidInfo: IAddress = {
   city: 'valid_city',
   state: 'valid_state',
@@ -9,6 +21,14 @@ export const addressValidInfo: IAddress = {
 }
 
 describe('Address', () => {
+  describe('Failure tests', () => {
+    it('01 - should fail length city less than 3', () => {
+      const cityLength2 = { ...addressValidInfo, city: 'ab' }
+      const address = new FctAddress(cityLength2)
+      expect(() => address.execute()).toThrowError('city should have more than 2 characters')
+    })
+  })
+
   describe('Successful tests', () => {
     it('01 - the class Address should exist', () => {
       expect(Address).toBeDefined()
