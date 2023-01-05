69e96778bb7f8acdab5358907a34ca4f41958e42
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-05 17:04:45 -0300

    refactor: :recycle: remove duplicated code
    write customerValidInfo once instead of many and remove duplicated code

--- back-end/src/app/entities/customer.test.ts
+++ back-end/src/app/entities/customer.test.ts
@@ -25,41 +25,31 @@ class FctCustomer {
   }
 }
 
+const customerValidInfo: ICurstomer = {
+  name: 'valid_name',
+  email: 'valid@email.com',
+  phone: '5527900000000',
+  address: 'address_test',
+  cpf: '99999999999'
+}
+
 describe('Customer', () => {
   describe('Failure tests', () => {
     it('01 - should fail when "name" has less than 3 characters', () => {
-      const customerInfo = {
-        name: '12',
-        email: 'email_test',
-        phone: 'phone_test',
-        address: 'address_test',
-        cpf: 'address_test'
-      }
+      const customerInfo = { ...customerValidInfo, name: 'ab' }
       const customer = new FctCustomer(customerInfo)
       expect(() => customer.execute()).toThrowError('Should have more than 2 characteres')
     })
 
     it('02 - should fail when pass invalid "email"', () => {
-      const customerInfo = {
-        name: 'name_test',
-        email: 'invalid_email_test',
-        phone: 'phone_test',
-        address: 'address_test',
-        cpf: 'address_test'
-      }
+      const customerInfo = { ...customerValidInfo, email: 'invalid_email' }
       const customer = new FctCustomer(customerInfo)
       jest.spyOn(customer.emailValidator, 'isValid').mockReturnValueOnce(false)
       expect(() => customer.execute()).toThrowError('Should be a valid email')
     })
 
     it('03 - should fail when pass an invalid "phone"', () => {
-      const customerInfo = {
-        name: 'name_test',
-        email: 'email_test',
-        phone: 'invalid_phone_test',
-        address: 'address_test',
-        cpf: 'address_test'
-      }
+      const customerInfo = { ...customerValidInfo, phone: 'invalid_phone' }
       const customer = new FctCustomer(customerInfo)
       expect(() => customer.execute()).toThrowError('Should have only numbers')
     })
@@ -67,26 +57,12 @@ describe('Customer', () => {
 
   describe('Successful tests', () => {
     it('01 - the class Customer should exist', () => {
-      const customerInfo = {
-        name: 'name_test',
-        email: 'email_test',
-        phone: 'phone_test',
-        address: 'address_test',
-        cpf: 'address_test'
-      }
-      const customer = new FctCustomer(customerInfo)
+      const customer = new FctCustomer(customerValidInfo)
       expect(customer).toBeTruthy()
     })
 
     it('02 - should not throw error when pass a valid customer', () => {
-      const customerInfo = {
-        name: 'valid_name',
-        email: 'valid_email',
-        phone: '5527900000000',
-        address: 'valid_address',
-        cpf: 'valid_cpf'
-      }
-      const customer = new FctCustomer(customerInfo)
+      const customer = new FctCustomer(customerValidInfo)
       expect(() => customer.execute()).not.toThrowError()
     })
   })
