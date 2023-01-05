a873bc484512bd7b8ae3f9eabaac22eca1536966
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-04 22:20:51 -0300

    test: :test_tube: create validation from name

--- back-end/src/app/entities/customer.test.ts
+++ back-end/src/app/entities/customer.test.ts
@@ -11,4 +11,16 @@ describe('Customer', () => {
     })
     expect(customer).toBeTruthy()
   })
+
+  it('02 - name should have more than 3 characteres', () => {
+    const customer = (): Customer => new Customer({
+      name: '12',
+      email: 'email_test',
+      phone: 'phone_test',
+      address: 'address_test',
+      cpf: 'address_test'
+    })
+
+    expect(customer).toThrowError()
+  })
 })
