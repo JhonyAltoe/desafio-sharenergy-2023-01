943ab1ae41b3933db5b47ea2bdbeb9829c535332
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-05 14:46:51 -0300

    test: :test_tube: create validation for email

--- back-end/src/app/entities/customer.test.ts
+++ back-end/src/app/entities/customer.test.ts
@@ -1,26 +1,69 @@
-import { Customer } from './Customer'
+import { EmailValidator } from '../protocols/emailValidator'
+import { Customer, ICurstomer } from './Customer'
+
+const fctEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (_email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  return emailValidatorStub
+}
+
+class FctCustomer {
+  emailValidator: EmailValidator
+  customerProp: ICurstomer
+
+  constructor (customerProp: ICurstomer) {
+    this.emailValidator = fctEmailValidator()
+    this.customerProp = customerProp
+  }
+
+  execute (): Customer {
+    return new Customer(this.customerProp, this.emailValidator)
+  }
+}
 
 describe('Customer', () => {
-  it('01 - the class Customer should exist', () => {
-    const customer = new Customer({
-      name: 'name_test',
-      email: 'email_test',
-      phone: 'phone_test',
-      address: 'address_test',
-      cpf: 'address_test'
+  describe('Failure tests', () => {
+    it('01 - should fail when "name" has less than 3 characters', () => {
+      const customerInfo = {
+        name: '12',
+        email: 'email_test',
+        phone: 'phone_test',
+        address: 'address_test',
+        cpf: 'address_test'
+      }
+      const customer = new FctCustomer(customerInfo)
+      expect(customer.execute).toThrowError()
     })
-    expect(customer).toBeTruthy()
+
+    it('02 - should fail when pass invalid "email"', () => {
+      const customerInfo = {
+        name: 'name_test',
+        email: 'invalid_email_test',
+        phone: 'phone_test',
+        address: 'address_test',
+        cpf: 'address_test'
+      }
+      const customer = new FctCustomer(customerInfo)
+      jest.spyOn(customer.emailValidator, 'isValid').mockReturnValueOnce(false)
+      expect(customer.execute).toThrowError()
+    })
   })
 
-  it('02 - name should have more than 3 characteres', () => {
-    const customer = (): Customer => new Customer({
-      name: '12',
-      email: 'email_test',
-      phone: 'phone_test',
-      address: 'address_test',
-      cpf: 'address_test'
+  describe('Successful tests', () => {
+    it('01 - the class Customer should exist', () => {
+      const customerInfo = {
+        name: 'name_test',
+        email: 'email_test',
+        phone: 'phone_test',
+        address: 'address_test',
+        cpf: 'address_test'
+      }
+      const customer = new FctCustomer(customerInfo)
+      expect(customer).toBeTruthy()
     })
-
-    expect(customer).toThrowError()
   })
 })
