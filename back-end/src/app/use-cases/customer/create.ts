04f5b7a22c657d65ce12a8762230a7d1555e5912
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-07 19:12:25 -0300

    build: remove the file create.ts into use-cases

--- back-end/src/app/use-cases/customer/create.ts
+++
@@ -1,6 +0,0 @@
-import { ICustomer } from '../../entities/Customer'
-import { ICustomerResponse } from '../../repositories/CustomerRepository'
-
-export interface ICreateCustomer {
-  create: (customer: ICustomer) => Promise<ICustomerResponse>
-}
