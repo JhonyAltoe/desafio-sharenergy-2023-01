320bc77aec3f993d85ad040446b25192a20e5ce3
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-08 16:38:08 -0300

    test: validation need to be called in address class

--- back-end/src/app/entities/address.test.ts
+++ back-end/src/app/entities/address.test.ts
@@ -24,23 +24,27 @@ describe('Address', () => {
   describe('Failure tests', () => {
     it('01 - should fail length city less than 3', () => {
       const cityLength2 = { ...addressValidInfo, city: 'ab' }
-      const address = new FctAddress(cityLength2)
-      expect(() => address.execute()).toThrowError('city should have more than 2 characters')
+      const address = new FctAddress(cityLength2).execute()
+      expect(address.validate()).toBeInstanceOf(Error)
+      expect(address.validate()?.message).toEqual('city should have more than 2 characters')
     })
 
     it('02 - should fail when state different of 2 characters', () => {
       const stateLength1 = { ...addressValidInfo, state: 'a' }
       const stateLength3 = { ...addressValidInfo, state: 'abc' }
-      const addressWithStateLenth1 = new FctAddress(stateLength1)
-      const addressWithStateLenth3 = new FctAddress(stateLength3)
-      expect(() => addressWithStateLenth1.execute()).toThrowError('state should have exact 2 charaters')
-      expect(() => addressWithStateLenth3.execute()).toThrowError('state should have exact 2 charaters')
+      const addressWithStateLenth1 = new FctAddress(stateLength1).execute()
+      const addressWithStateLenth3 = new FctAddress(stateLength3).execute()
+      expect(addressWithStateLenth1.validate()).toBeInstanceOf(Error)
+      expect(addressWithStateLenth3.validate()).toBeInstanceOf(Error)
+      expect(addressWithStateLenth1.validate()?.message).toEqual('state should have exact 2 charaters')
+      expect(addressWithStateLenth3.validate()?.message).toEqual('state should have exact 2 charaters')
     })
 
     it('03 - should fail when street is empty', () => {
       const streetEmpty = { ...addressValidInfo, street: '' }
-      const address = new FctAddress(streetEmpty)
-      expect(() => address.execute()).toThrowError('street shouldn\'t be empty')
+      const address = new FctAddress(streetEmpty).execute()
+      expect(address.validate()).toBeInstanceOf(Error)
+      expect(address.validate()?.message).toEqual('street shouldn\'t be empty')
     })
   })
 
@@ -52,7 +56,7 @@ describe('Address', () => {
     it('02 - should pass when receive valid address', () => {
       const addressProps = { ...addressValidInfo }
       const address = new FctAddress(addressProps)
-      expect(address).toBeTruthy()
+      expect(address.execute()).toBeTruthy()
     })
 
     it('03 - should return full address', () => {
