a48ea552ef8ccb5fe1ea84f98c68b2215013cca5
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-09 13:40:42 -0300

    test: declare GetOneCustomer

---
+++ back-end/src/app/use-cases/customer-use-cases/get-one-customer.test.ts
@@ -0,0 +1,9 @@
+import { GetOneCustomer } from './get-one-customer'
+
+describe('use-cases/GetOneCustomer', () => {
+  describe('Successful tests', () => {
+    it('01 - should exist GetOneCustomer class', () => {
+      expect(GetOneCustomer).toBeDefined()
+    })
+  })
+})
