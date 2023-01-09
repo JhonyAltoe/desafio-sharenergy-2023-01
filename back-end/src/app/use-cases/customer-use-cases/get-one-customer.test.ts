a6e8756c77dc99da27247085c3170cf4c6e1d0f8
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-09 20:10:40 -0300

    test: create method getOne

--- back-end/src/app/use-cases/customer-use-cases/get-one-customer.test.ts
+++ back-end/src/app/use-cases/customer-use-cases/get-one-customer.test.ts
@@ -1,9 +1,29 @@
 import { GetOneCustomer } from './get-one-customer'
+import { FactoryGetOneCustomer } from '../factories/factory-get-one-customer'
+import { customersMock } from './in-memory-customer-repository/mock/customers-mock'
+import { CustomerResponse } from '../../repositories/customer-repository'
 
 describe('use-cases/GetOneCustomer', () => {
   describe('Successful tests', () => {
     it('01 - should exist GetOneCustomer class', () => {
       expect(GetOneCustomer).toBeDefined()
+    })
+
+    it('02 - should get one customer by email', async () => {
+      const factoryGetOneCustomer = new FactoryGetOneCustomer(customersMock())
+      const getOneCustomer = factoryGetOneCustomer.execute()
+      const customer = await getOneCustomer.getOne('joao@email.com') as CustomerResponse
+      expect(customer.email).toBe('joao@email.com')
+    })
+  })
+
+  describe('Failure tests', () => {
+    it('01 - should fail one when email don\'t exists', async () => {
+      const factoryGetOneCustomer = new FactoryGetOneCustomer(customersMock())
+      const getOneCustomer = factoryGetOneCustomer.execute()
+      const customer = await getOneCustomer.getOne('donotexists@email.com') as Error
+      expect(customer).toBeInstanceOf(Error)
+      expect(customer.message).toBe('customer do not exist in database')
     })
   })
 })
