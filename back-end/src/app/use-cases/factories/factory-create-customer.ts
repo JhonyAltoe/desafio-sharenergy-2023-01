e0f198ead8e4bceb79ee17723f5e157b97fe0b49
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-09 19:55:18 -0300

    refactor: change repository interface

--- back-end/src/app/use-cases/factories/factory-create-customer.ts
+++ back-end/src/app/use-cases/factories/factory-create-customer.ts
@@ -1,11 +1,11 @@
 import { fctEmailValidator } from '../../entities/customer.test'
 import { EmailValidator } from '../../protocols/emailValidator'
-import { CustomerResponse } from '../../repositories/customer-repository'
+import { CustomerRepository, CustomerResponse } from '../../repositories/customer-repository'
 import { CreateCustomer, ICreateCustomer } from '../customer-use-cases/create-customer'
 import { InMemoryCustomerRepository } from '../customer-use-cases/in-memory-customer-repository/in-memory-customer-repository'
 
 export class FactoryCreateCustomer {
-  customerRepository: InMemoryCustomerRepository
+  customerRepository: CustomerRepository
   emailValidator: EmailValidator
 
   constructor (dbCustomersMock: CustomerResponse[]) {
