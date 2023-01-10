8ba5cfc9d6873ed3dae6f5fd604832c5d311f224
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-10 14:45:35 -0300

    feat: implemente update method

--- back-end/src/app/use-cases/customer-use-cases/update-customer.ts
+++ back-end/src/app/use-cases/customer-use-cases/update-customer.ts
@@ -1,10 +1,24 @@
-import { CustomerProps } from '../../entities/customer'
-import { CustomerResponse } from '../../repositories/customer-repository'
+import { EmailValidator } from '../../protocols/emailValidator'
+import { CustomerRepository, CustomerResponse, UpdateCustomerProps } from '../../repositories/customer-repository'
 
 export interface IUpdateCustomer {
-  update: (id: string, customerProps: Partial<CustomerProps>) => Promise<CustomerResponse | Error >
+  update: (id: string, customerProps: UpdateCustomerProps) => Promise<CustomerResponse | Error >
 }
 
 export class UpdateCustomer implements IUpdateCustomer {
-  update: (id: string, customerProps: Partial<CustomerProps>) => Promise<CustomerResponse | Error>
+  private readonly customerRepository: CustomerRepository
+  private readonly emailValidator: EmailValidator
+
+  constructor (customerRepository: CustomerRepository, emailValidator: EmailValidator) {
+    this.customerRepository = customerRepository
+    this.emailValidator = emailValidator
+  }
+
+  async update (id: string, customerProps: UpdateCustomerProps): Promise<CustomerResponse | Error> {
+    const updatedCustomerOrNull = await this.customerRepository.update(id, customerProps)
+    if (updatedCustomerOrNull === null) {
+      return Error('customer don\'t exists in database')
+    }
+    return updatedCustomerOrNull
+  }
 }
