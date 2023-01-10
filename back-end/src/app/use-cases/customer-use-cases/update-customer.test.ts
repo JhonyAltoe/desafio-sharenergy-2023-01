07145c40e7ceec3f05e9551c3561fa725c3b47af
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-10 13:03:02 -0300

    test: define UpdateCustomer

---
+++ back-end/src/app/use-cases/customer-use-cases/update-customer.test.ts
@@ -0,0 +1,9 @@
+import { UpdateCustomer } from './update-customer'
+
+describe('use-cases/UpdateCustomer', () => {
+  describe('Successful tests', () => {
+    it('01 - should exist UpdateCustomer class', () => {
+      expect(UpdateCustomer).toBeDefined()
+    })
+  })
+})
