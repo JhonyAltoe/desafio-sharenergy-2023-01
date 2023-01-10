a655fe193f7bcd674421e58afbde3dd75cb8950f
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-10 14:45:58 -0300

    test: implemente update method

--- back-end/src/app/use-cases/customer-use-cases/update-customer.test.ts
+++ back-end/src/app/use-cases/customer-use-cases/update-customer.test.ts
@@ -1,9 +1,25 @@
+import { CustomerResponse, UpdateCustomerProps } from '../../repositories/customer-repository'
+import { FactoryUpdateCustomer } from '../factories-for-testing/factory-update-customer'
+import { customersMock } from './in-memory-customer-repository/mock/customers-mock'
 import { UpdateCustomer } from './update-customer'
 
 describe('use-cases/UpdateCustomer', () => {
   describe('Successful tests', () => {
     it('01 - should exist UpdateCustomer class', () => {
       expect(UpdateCustomer).toBeDefined()
+    })
+
+    it('02 - should return an updated customer', async () => {
+      const updateCustomerProps: UpdateCustomerProps = {
+        email: 'joaodasilva@email.com',
+        address: { state: 'vitória' }
+      }
+
+      const factoryUpdateCustomer = new FactoryUpdateCustomer(customersMock())
+      const updateCustomer = factoryUpdateCustomer.execute()
+      const updatedCustomer = await updateCustomer.update('uuid-1', updateCustomerProps) as CustomerResponse
+      expect(updatedCustomer.email).toBe('joaodasilva@email.com')
+      expect(updatedCustomer.address.state).toBe('vitória')
     })
   })
 })
