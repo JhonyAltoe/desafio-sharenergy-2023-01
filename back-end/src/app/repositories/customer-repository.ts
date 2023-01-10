e382be0ee47d1271a2c6ea43284c1c2fdd5c310d
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-10 14:40:10 -0300

    feat: create new interface to update method - customer repository

--- back-end/src/app/repositories/customer-repository.ts
+++ back-end/src/app/repositories/customer-repository.ts
@@ -1,5 +1,21 @@
 import { CustomerProps } from '../entities/customer'
 
+interface UpdateAddressProps {
+  city?: string
+  state?: string
+  street?: string
+  number?: string
+  comment?: string
+}
+
+export interface UpdateCustomerProps {
+  name?: string
+  email?: string
+  phone?: string
+  address?: UpdateAddressProps
+  cpf?: string
+}
+
 export interface CustomerRequest extends CustomerProps {
   id?: string
 }
@@ -12,6 +28,6 @@ export interface CustomerRepository {
   create: (customer: CustomerRequest) => Promise<CustomerResponse>
   remove: (id: string) => Promise<void>
   getByEmail: (email: string) => Promise<CustomerResponse | null>
-  update: (id: string, customer: Partial<CustomerRequest>) => Promise<CustomerResponse | null>
+  update: (id: string, customer: UpdateCustomerProps) => Promise<CustomerResponse | null>
   exists: (email: string) => Promise<Boolean>
 }
