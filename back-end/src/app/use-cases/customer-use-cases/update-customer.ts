ab904231d3ac583b749c63cfa93a466d3137d83f
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-11 13:20:28 -0300

    feat: add verification

--- back-end/src/app/use-cases/customer-use-cases/update-customer.ts
+++ back-end/src/app/use-cases/customer-use-cases/update-customer.ts
@@ -1,5 +1,4 @@
-import { PartialCustomerProps } from '../../entities/validations/customer-validator'
-import { EmailValidator } from '../../protocols/emailValidator'
+import { ICustomerValidator, PartialCustomerProps } from '../../entities/validations/customer-validator'
 import { CustomerRepository, CustomerResponse } from '../../repositories/customer-repository'
 
 export interface IUpdateCustomer {
@@ -8,14 +7,19 @@ export interface IUpdateCustomer {
 
 export class UpdateCustomer implements IUpdateCustomer {
   private readonly customerRepository: CustomerRepository
-  private readonly emailValidator: EmailValidator
+  private readonly customerValidator: ICustomerValidator
 
-  constructor (customerRepository: CustomerRepository, emailValidator: EmailValidator) {
+  constructor (customerRepository: CustomerRepository, customerValidator: ICustomerValidator) {
     this.customerRepository = customerRepository
-    this.emailValidator = emailValidator
+    this.customerValidator = customerValidator
   }
 
   async update (id: string, customerProps: PartialCustomerProps): Promise<CustomerResponse | Error> {
+    const errorOrUndefined = this.customerValidator.validateCustomer(customerProps)
+    if (errorOrUndefined instanceof Error) {
+      return errorOrUndefined
+    }
+
     const updatedCustomerOrNull = await this.customerRepository.update(id, customerProps)
     if (updatedCustomerOrNull === null) {
       return Error('customer don\'t exists in database')
