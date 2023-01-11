f0061a59867423497184aae32b2392b0b555cf44
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-11 13:19:10 -0300

    refactor: add customer validator

--- back-end/src/app/use-cases/factories-for-testing/factory-update-customer.ts
+++ back-end/src/app/use-cases/factories-for-testing/factory-update-customer.ts
@@ -1,19 +1,19 @@
 import { factoryEmailValidator } from '../../entities/factories-for-testing/factory-email-validator'
-import { EmailValidator } from '../../protocols/emailValidator'
+import { CustomerValidator, ICustomerValidator } from '../../entities/validations/customer-validator'
 import { CustomerRepository, CustomerResponse } from '../../repositories/customer-repository'
 import { InMemoryCustomerRepository } from '../customer-use-cases/in-memory-customer-repository/in-memory-customer-repository'
 import { IUpdateCustomer, UpdateCustomer } from '../customer-use-cases/update-customer'
 
 export class FactoryUpdateCustomer {
   customerRepository: CustomerRepository
-  emailValidator: EmailValidator
+  customerValidator: ICustomerValidator
 
   constructor (dbCustomersMock: CustomerResponse[]) {
     this.customerRepository = new InMemoryCustomerRepository(dbCustomersMock)
-    this.emailValidator = factoryEmailValidator()
+    this.customerValidator = new CustomerValidator(factoryEmailValidator())
   }
 
   execute (): IUpdateCustomer {
-    return new UpdateCustomer(this.customerRepository, this.emailValidator)
+    return new UpdateCustomer(this.customerRepository, this.customerValidator)
   }
 }
