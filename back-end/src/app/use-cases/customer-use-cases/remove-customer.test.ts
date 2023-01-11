6fa7a251e0d39df4184e00da12638d33c33b26f3
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-11 13:30:21 -0300

    test: define RemoveCustomer - use-cases

---
+++ back-end/src/app/use-cases/customer-use-cases/remove-customer.test.ts
@@ -0,0 +1,9 @@
+import { RemoveCustomer } from './remove-customer'
+
+describe('use-cases/DeleteCustomer', () => {
+  describe('Successful tests', () => {
+    it('01 - should exists', () => {
+      expect(RemoveCustomer).toBeDefined()
+    })
+  })
+})
