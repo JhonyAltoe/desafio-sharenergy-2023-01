60381811dd0ae6770c85930f1b01dba8e99a4a95
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-11 13:50:53 -0300

    test: implement remove method

--- back-end/src/app/use-cases/customer-use-cases/remove-customer.test.ts
+++ back-end/src/app/use-cases/customer-use-cases/remove-customer.test.ts
@@ -1,9 +1,19 @@
+import { FactoryRemoveCustomer } from '../factories-for-testing/factory-remove-customer'
+import { customersMock } from './in-memory-customer-repository/mock/customers-mock'
 import { RemoveCustomer } from './remove-customer'
 
 describe('use-cases/DeleteCustomer', () => {
   describe('Successful tests', () => {
     it('01 - should exists', () => {
       expect(RemoveCustomer).toBeDefined()
+    })
+
+    it('02 - should remove by id', async () => {
+      const factoryRemoveCustomer = new FactoryRemoveCustomer(customersMock())
+      const removeCustomer = factoryRemoveCustomer.execute()
+      await removeCustomer.remove('uuid-2')
+      const customersInRepo = factoryRemoveCustomer.customerRepository.customers
+      expect(customersInRepo).toHaveLength(1)
     })
   })
 })
