b2907a0b867f3756803a2923fc9ec92c3c136258
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-11 23:32:51 -0300

    test: declare CreateCustomerController

---
+++ back-end/src/presentation/controllers/customer-controllers/create-customer-controller.test.ts
@@ -0,0 +1,9 @@
+import { CreateCustomerController } from './create-customer-controller'
+
+describe('CreateCustomerController', () => {
+  describe('Sucessfull tests', () => {
+    it('01 - should exist', () => {
+      expect(CreateCustomerController).toBeDefined()
+    })
+  })
+})
