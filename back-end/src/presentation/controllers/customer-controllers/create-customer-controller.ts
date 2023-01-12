e55ca78f3211b0c49a2bded22be7bf3af816955c
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-12 15:36:27 -0300

    feat: implement CreateCustomerController

--- back-end/src/presentation/controllers/customer-controllers/create-customer-controller.ts
+++ back-end/src/presentation/controllers/customer-controllers/create-customer-controller.ts
@@ -1,6 +1,28 @@
+import { ICreateCustomer } from '../../../domain/use-case-interfaces/customer-use-cases/create-customer'
+import { InvalidParamError } from '../../errors/invalid-param-error'
+import { badRequest, ok } from '../../helpers/http-helper'
+import { isCustomerEmpty } from '../../helpers/isCustomerEmpty'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class CreateCustomerController implements Controller {
-  handler: (httpRequest: HttpRequest) => Promise<HttpResponse>
+  createCustomer: ICreateCustomer
+
+  constructor (createCustomer: ICreateCustomer) {
+    this.createCustomer = createCustomer
+  }
+
+  async handler (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const missingParamError = isCustomerEmpty(httpRequest.body)
+    if (missingParamError !== null) {
+      return badRequest(missingParamError)
+    }
+
+    const customerOrError = await this.createCustomer.create(httpRequest.body)
+    if (customerOrError instanceof Error) {
+      return badRequest(new InvalidParamError(customerOrError.message))
+    }
+
+    return ok(customerOrError)
+  }
 }
