f40d489b851bb2d247d3678f4c14a68c75cd3d7d
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-12 20:52:28 -0300

    feat: insert getOneCustomer into GetOneCustomerController

--- back-end/src/presentation/controllers/customer-controllers/get-one-customer-controller.ts
+++ back-end/src/presentation/controllers/customer-controllers/get-one-customer-controller.ts
@@ -1,6 +1,7 @@
 import { IGetOneCustomer } from '../../../domain/use-case-interfaces/customer-use-cases/get-one-customer'
 import { MissingParamError } from '../../errors/missing-param-error'
-import { badRequest, ok } from '../../helpers/http-helper'
+import { NotFound } from '../../errors/not-found-error'
+import { badRequest, notFound, ok } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
@@ -16,6 +17,11 @@ export class GetOneCustomerController implements Controller {
       return badRequest(new MissingParamError('email'))
     }
 
-    return ok({ body: 'test' })
+    const customerOrError = await this.getOneCustomer.getOne(httpRequest.param)
+    if (customerOrError instanceof Error) {
+      return notFound(new NotFound(customerOrError))
+    }
+
+    return ok(customerOrError)
   }
 }
