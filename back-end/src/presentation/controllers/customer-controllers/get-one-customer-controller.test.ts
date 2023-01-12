eb665b6659d3f17f74a174d900447bda7ef41724
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-12 20:53:01 -0300

    test: insert getOneCustomer into GetOneCustomerController

--- back-end/src/presentation/controllers/customer-controllers/get-one-customer-controller.test.ts
+++ back-end/src/presentation/controllers/customer-controllers/get-one-customer-controller.test.ts
@@ -1,3 +1,4 @@
+import { customerResponse } from '../../../domain/entities/customer.test'
 import { FactoryGetOneCustomerController } from '../factories-for-testing/factory-getone-customer-controller'
 import { GetOneCustomerController } from './get-one-customer-controller'
 
@@ -9,6 +10,19 @@ describe('GetOneCustomerController', () => {
 
     it('02 - should return status 200 and customer', async () => {
       const request = {
+        param: 'joao@email.com'
+      }
+
+      const createCustomerController = new FactoryGetOneCustomerController().execute()
+      const response = await createCustomerController.handler(request)
+      expect(response.statusCode).toEqual(200)
+      expect(response.body).toEqual({ data: customerResponse })
+    })
+  })
+
+  describe('Failure tests body { message: \'missing parameter: email\' }', () => {
+    it('01 - should return status 400 and ', async () => {
+      const request = {
         param: ''
       }
 
@@ -16,6 +30,19 @@ describe('GetOneCustomerController', () => {
       const response = await createCustomerController.handler(request)
       expect(response.statusCode).toEqual(400)
       expect(response.body).toEqual({ message: 'missing parameter: email' })
+    })
+
+    it('01 - should return status 400 and ', async () => {
+      const request = {
+        param: 'joao@email.com'
+      }
+
+      const factory = new FactoryGetOneCustomerController()
+      jest.spyOn(factory.getOneCustomer, 'getOne').mockResolvedValue(new Error('error for testing'))
+      const createCustomerController = factory.execute()
+      const response = await createCustomerController.handler(request)
+      expect(response.statusCode).toEqual(404)
+      expect(response.body).toEqual({ message: 'error for testing' })
     })
   })
 })
