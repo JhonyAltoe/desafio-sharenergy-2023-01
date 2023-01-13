71f87e24a1c5241d64de6dea615a4b33d917e809
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-13 11:05:01 -0300

    refactor: switch default expor to export

--- back-end/src/infra/repositories/connect.ts
+++ back-end/src/infra/repositories/connect.ts
@@ -2,7 +2,5 @@ import mongoose from 'mongoose'
 
 const MONGO_DB_URL = 'mongodb://localhost:27017/sharenergy'
 
-const connectToDatabase = async (mongoDatabaseURI = MONGO_DB_URL): Promise<typeof mongoose> =>
+export const connectToDatabase = async (mongoDatabaseURI = MONGO_DB_URL): Promise<typeof mongoose> =>
   await mongoose.connect(mongoDatabaseURI)
-
-export default connectToDatabase
