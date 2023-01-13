3f824606d8ec5032f39f62ca45fc46eadc270c9c
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-13 11:05:51 -0300

    refactor: separete address in a new constant

--- back-end/src/infra/repositories/customer.ts
+++ back-end/src/infra/repositories/customer.ts
@@ -1,17 +1,20 @@
 import { Schema } from 'mongoose'
 import { CustomerProps } from '../../domain/entities/customer'
+import { AddressProps } from '../../domain/entities/address'
+
+const addressMongooseSchema = new Schema<AddressProps>({
+  city: String,
+  state: String,
+  street: String,
+  number: String,
+  comment: String
+})
 
 export const customerMongooseSchema = new Schema<CustomerProps>({
   name: String,
   email: String,
   phone: String,
-  address: {
-    city: String,
-    state: String,
-    street: String,
-    number: String,
-    comment: String
-  },
+  address: addressMongooseSchema,
   cpf: String
 }, { versionKey: false })
 
