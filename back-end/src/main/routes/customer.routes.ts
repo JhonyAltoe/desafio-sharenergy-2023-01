c5f5ba609c0591c5d66be929c78941d1bae293ea
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-14 15:05:56 -0300

    feat: create update route

--- back-end/src/main/routes/customer.routes.ts
+++ back-end/src/main/routes/customer.routes.ts
@@ -2,7 +2,8 @@ import express from 'express'
 import { adaptRoute } from '../adapters/adapt-route-controller'
 
 import {
-  makeCreateCustomerController, makeGetOneCustomerController, makeRemoveCustomerController
+  makeCreateCustomerController, makeGetOneCustomerController,
+  makeRemoveCustomerController, makeUpdateCustomerController
 } from '../factories'
 
 const customerRoute = express.Router()
@@ -10,5 +11,6 @@ const customerRoute = express.Router()
 customerRoute.route('/criar').post(adaptRoute(makeCreateCustomerController()))
 customerRoute.route('/:email').get(adaptRoute(makeGetOneCustomerController()))
 customerRoute.route('/:id').delete(adaptRoute(makeRemoveCustomerController()))
+customerRoute.route('/:id').patch(adaptRoute(makeUpdateCustomerController()))
 
 export { customerRoute }
