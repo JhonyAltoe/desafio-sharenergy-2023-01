643ff794f09ee4cb651190f1f5bbb738ee687160
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-14 00:03:28 -0300

    test: declare UpdateCustomerController

---
+++ back-end/src/presentation/controllers/customer-controllers/update-customer-controller.test.ts
@@ -0,0 +1,9 @@
+import { UpdateCustomerController } from './update-customer-controller'
+
+describe('UpdateCustomerController', () => {
+  describe('Sucessful tests', () => {
+    it('01 - should exists', () => {
+      expect(UpdateCustomerController).toBeDefined()
+    })
+  })
+})
