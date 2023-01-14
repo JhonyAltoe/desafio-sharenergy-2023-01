9b22abd56591c2f469c68a3b3ed14259b8478828
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-14 13:23:43 -0300

    feat: create index for exporting

---
+++ back-end/src/main/factories/index.ts
@@ -0,0 +1,4 @@
+export * from './make-create-customer-controller'
+export * from './make-getone-customer-controller'
+export * from './make-remove-customer-controller'
+export * from './make-update-customer-controller'
