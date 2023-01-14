ddd67395dae0912f8d6380b1b6dcb8af86934f87
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-14 13:27:19 -0300

    feat: create route for get one customer

--- back-end/src/main/routes/customer.routes.ts
+++ back-end/src/main/routes/customer.routes.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import { adaptRoute } from '../adapters/adapt-route-controller'
-import { makeCreateCustomerController } from '../factories/make-create-customer-controller'
+
+import {
+  makeCreateCustomerController, makeGetOneCustomerController
+} from '../factories'
 
 const customerRoute = express.Router()
 
 customerRoute.route('/criar').post(adaptRoute(makeCreateCustomerController()))
+customerRoute.route('/:email').get(adaptRoute(makeGetOneCustomerController()))
 
 export { customerRoute }
