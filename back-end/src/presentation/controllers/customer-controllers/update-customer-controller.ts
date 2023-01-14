6865646b0498533199fad6d2579e1875bec3efca
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-14 15:04:05 -0300

    feat: insert try catch in controller

--- back-end/src/presentation/controllers/customer-controllers/update-customer-controller.ts
+++ back-end/src/presentation/controllers/customer-controllers/update-customer-controller.ts
@@ -1,7 +1,8 @@
 import { IUpdateCustomer } from '../../../domain/use-case-interfaces/customer-use-cases/update-customer'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { NotFound } from '../../errors/not-found-error'
-import { badRequest, notFound, ok } from '../../helpers/http-helper'
+import { ServerError } from '../../errors/server-error'
+import { badRequest, notFound, ok, serverError } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
@@ -13,20 +14,23 @@ export class UpdateCustomerController implements Controller {
   }
 
   async handler (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { param, body } = httpRequest
-    if ([undefined, ''].some(e => e === param.id)) {
-      return badRequest(new MissingParamError('id'))
-    }
+    try {
+      const { param, body } = httpRequest
+      if ([undefined, ''].some(e => e === param.id)) {
+        return badRequest(new MissingParamError('id'))
+      }
 
-    if (Object.keys(body).length === 0) {
-      return badRequest(new MissingParamError('body'))
+      if (Object.keys(body).length === 0) {
+        return badRequest(new MissingParamError('body'))
+      }
+      const updatedOrError = await this.updateCustomer.update(httpRequest.param.id, httpRequest.body)
+      if (updatedOrError instanceof Error) {
+        return notFound(new NotFound(updatedOrError))
+      }
+      return ok(updatedOrError)
+    } catch (error) {
+      console.error(error)
+      return serverError(new ServerError('internal server error'))
     }
-
-    const updatedOrError = await this.updateCustomer.update(httpRequest.param.id, httpRequest.body)
-    if (updatedOrError instanceof Error) {
-      return notFound(new NotFound(updatedOrError))
-    }
-
-    return ok(updatedOrError)
   }
 }
