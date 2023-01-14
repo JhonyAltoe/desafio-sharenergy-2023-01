5261313b75de6a962b9e33c3f3047c0879dea34e
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-14 13:26:05 -0300

    fix: insert email key into param

--- back-end/src/presentation/controllers/customer-controllers/get-one-customer-controller.test.ts
+++ back-end/src/presentation/controllers/customer-controllers/get-one-customer-controller.test.ts
@@ -10,31 +10,35 @@ describe('GetOneCustomerController', () => {
 
     it('02 - should return status 200 and customer', async () => {
       const request = {
-        param: 'joao@email.com'
+        param: {
+          email: 'joao@email.com'
+        }
       }
 
-      const createCustomerController = new FactoryGetOneCustomerController().execute()
-      const response = await createCustomerController.handler(request)
+      const getOneCustomerController = new FactoryGetOneCustomerController().execute()
+      const response = await getOneCustomerController.handler(request)
       expect(response.statusCode).toEqual(200)
       expect(response.body).toEqual({ data: customerResponse })
     })
   })
 
-  describe('Failure tests body { message: \'missing parameter: email\' }', () => {
-    it('01 - should return status 400 and ', async () => {
+  describe('Failure tests', () => {
+    it('01 - should return status 400 and body { message: \'missing parameter: email\' }', async () => {
       const request = {
-        param: ''
+        param: {}
       }
 
-      const createCustomerController = new FactoryGetOneCustomerController().execute()
-      const response = await createCustomerController.handler(request)
+      const getOneCustomerController = new FactoryGetOneCustomerController().execute()
+      const response = await getOneCustomerController.handler(request)
       expect(response.statusCode).toEqual(400)
       expect(response.body).toEqual({ message: 'missing parameter: email' })
     })
 
-    it('01 - should return status 400 and ', async () => {
+    it('02 - should return status 404 and { message: \'error for testing\' }', async () => {
       const request = {
-        param: 'joao@email.com'
+        param: {
+          email: 'joao@email.com'
+        }
       }
 
       const factory = new FactoryGetOneCustomerController()
