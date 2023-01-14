343cb91e14b95dd9cd2b3ddced0eb980bd4eccc2
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-14 14:28:15 -0300

    feat: add route remove customer

--- back-end/src/main/routes/customer.routes.ts
+++ back-end/src/main/routes/customer.routes.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import { adaptRoute } from '../adapters/adapt-route-controller'
 
 import {
-  makeCreateCustomerController, makeGetOneCustomerController
+  makeCreateCustomerController, makeGetOneCustomerController, makeRemoveCustomerController
 } from '../factories'
 
 const customerRoute = express.Router()
 
 customerRoute.route('/criar').post(adaptRoute(makeCreateCustomerController()))
 customerRoute.route('/:email').get(adaptRoute(makeGetOneCustomerController()))
+customerRoute.route('/:id').delete(adaptRoute(makeRemoveCustomerController()))
 
 export { customerRoute }
