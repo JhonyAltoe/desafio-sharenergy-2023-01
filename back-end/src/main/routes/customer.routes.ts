9aaf068ec14fdaef992ab29181a70032c920b7df
Jhony Mike Altoé <jhonymikealtoe@hotmail.com>
2023-01-16 23:56:36 -0300

    feat: move auth

--- back-end/src/main/routes/customer.routes.ts
+++ back-end/src/main/routes/customer.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { adaptRoute } from '../adapters/adapt-route-controller'
+import { authToken } from '../middlewares/auth'
 
 import {
   makeCreateCustomerController, makeGetAllCustomerController, makeGetOneCustomerController,
@@ -9,9 +10,9 @@ import {
 const customerRoute = express.Router()
 
 customerRoute.route('/create').post(adaptRoute(makeCreateCustomerController()))
-customerRoute.route('/getone/:email').get(adaptRoute(makeGetOneCustomerController()))
-customerRoute.route('/getall').get(adaptRoute(makeGetAllCustomerController()))
-customerRoute.route('/remove:id').delete(adaptRoute(makeRemoveCustomerController()))
-customerRoute.route('/update:id').patch(adaptRoute(makeUpdateCustomerController()))
+customerRoute.route('/getone/:email').get(authToken, adaptRoute(makeGetOneCustomerController()))
+customerRoute.route('/getall').get(authToken, adaptRoute(makeGetAllCustomerController()))
+customerRoute.route('/remove:id').delete(authToken, adaptRoute(makeRemoveCustomerController()))
+customerRoute.route('/update:id').patch(authToken, adaptRoute(makeUpdateCustomerController()))
 
 export { customerRoute }
--- back-end/src/main/routes/router.ts
+++ back-end/src/main/routes/router.ts
@@ -1,12 +1,11 @@
 import express from 'express'
 import { customerRoute } from './customer.routes'
 import { userRoutes } from './user.routes'
-import { authToken } from '../middlewares/auth'
 import { authenticationRoute } from './authentication.routes'
 
 const router = express.Router()
 
-router.use('/customer', authToken, customerRoute)
+router.use('/customer', customerRoute)
 router.use('/user', userRoutes)
 router.use('/auth', authenticationRoute)
 
